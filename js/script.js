70c0260772126fd7a1978d92789f2c9f5037e0ba
Miloserdie <89547280+Miloserdie@users.noreply.github.com>
2022-06-15 15:40:29 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,10 +11,9 @@ function fromTo(a, b){
 	}
 }
 fromTo(10, 1);
-window.onload = function(){
-	window.setInterval(function(){
-		let now = new Date();
-		let clock = document.getElementById("clock");
-		clock.innerHTML = now.toLocaleTimeString();
+onload = function(){
+	let clock = document.querySelector('#clock');
+	setInterval(function(){
+		clock.innerHTML = new Date().toLocaleTimeString();
 	}, 1000);
 };
